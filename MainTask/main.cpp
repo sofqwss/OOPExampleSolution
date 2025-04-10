e6e9ad08a5147e17a920de4fc8303b1728dc43f9
Sofia Woronowicz <emiliavoronovic@gmail.com>
2025-04-10 18:53:27 +0300

    constructor

--- MainTask/main.cpp
+++ MainTask/main.cpp
@@ -1,33 +1,22 @@
-#include "students.h"
+﻿﻿#include "Student.h"
 
 int main() {
-	Student* st1 = nullptr;
-	Student* st2 = nullptr;
+	//int array[10]{ 1,2,3 };
 
-	st1 = new Student;
+	Student student1;
+	Student student2("Alex");
+	Student student3("Matvey", 14);
+	Student student4("Bogdan", 15, 10, true);
+	Student student5(student4);
 
-	st1->name = "Veronika";
-	st1->age = 14;
-	st1->mark = 9.2;
-	st1->alive = true;
-	   
-	st2->name = "Danik";
-	st2->name = 15;
-	st2->mark = 9.99;
-	st2->alive = false;
+	//Student student5 = student4;
 
+	cout << student1.toString() << endl;
+	cout << student2.toString() << endl;
+	cout << student3.toString() << endl;
+	cout << student4.toString() << endl;
+	cout << student5.toString() << endl;
 
-	cout << "Before: " << endl;
-	cout << st1->getString() << endl;
-	cout << st2->getString() << endl;
-	
-	st1 = st2;
-	st1->name = "Bogdan";
-
-	cout << "Before: " << endl;
-	cout << st1->getString() << endl;
-	cout << st2->getString() << endl;
-		
 
 	return 0;
 }
--- MainTask/students.h
+++ MainTask/students.h
@@ -4,21 +4,75 @@ using namespace std;
 
 class Student {
 public:
-	//fields
+	// fields
 	string name;
 	int age;
 	double mark;
 	bool alive;
 
-	//methods
-	string getString() {
+	// constructors
+
+	// default-constructors or constructors without arguments
+	Student() {
+		cout << "default-constructor..." << endl;
+		name = "no name";
+		age = 13;
+		mark = 4.0;
+		alive = true;
+	}
+
+	// constructor with arguments
+	Student(string nm) {
+		cout << "constructor with arguments (name)..." << endl;
+		name = nm;
+		age = 13;
+		mark = 4.0;
+		alive = true;
+	}
+
+	// constructor with arguments
+	Student(string nm, int a) {
+		cout << "constructor with arguments (name, age)..." << endl;
+		name = nm;
+		age = a < 13 ? 13 : a;
+		mark = 4.0;
+		alive = true;
+	}
+
+	Student(int a, string nm) {
+		cout << "constructor with arguments (name, age)..." << endl;
+		name = nm;
+		age = a < 13 ? 13 : a;
+		mark = 4.0;
+		alive = true;
+	}
+
+	// canonical-constructor
+	Student(string nm, int a, double m, bool alv) {
+		cout << "canonical-constructor ..." << endl;
+		name = nm;
+		age = a;
+		mark = m;
+		alive = alv;
+	}
+
+	// copy-constructor
+	Student(const Student& student) {
+		cout << "copy-constructor ..." << endl;
+		name = student.name;
+		age = student.age;
+		mark = student.mark;
+		alive = student.alive;
+	}
+
+	// methods
+	string toString() {
 		string s = "Name: " + name;
 		s += ", age: " + to_string(age);
 		s += ", average mark: " + to_string(mark);
 		s += ", alive: ";
 		s += alive ? "yes" : "no";
 		return s;
-
 	}
-	
+
 };
